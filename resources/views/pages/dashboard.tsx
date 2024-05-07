cdc6593ee15d92e1c36fecfb2ebbe69caaafb130
YannickOclock <yannick.kuhn@oclock.io>
2024-05-07 08:49:38 +0200

    Mise à jour styles

--- resources/views/pages/dashboard.tsx
+++ resources/views/pages/dashboard.tsx
@@ -10,6 +10,31 @@ export function Dashboard() {
         <header>
           <h2>Welcome to Dashboard</h2>
         </header>
+        <section class="main-full">
+          <div>
+            <h2>Formulaire de recherche</h2>
+            <div class="crumble-card">
+              <div class="crumble-no-course-card">
+                <form action="" method="get">
+                  <div class="form-group">
+                    <label for="search">Taper le nom de la promo recherchée</label>
+                    <input type="text" name="search" id="search" class="form-control" />
+                  </div>
+                  <div class="form-group-divided">
+                    <div class="form-group">
+                      <label for="search">Date de début</label>
+                      <input type="date" name="start_date" id="start_date" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                      <label for="search">Date de fin</label>
+                      <input type="date" name="end_date" id="end_date" class="form-control" />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </App>
   )
